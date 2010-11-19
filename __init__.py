14cdeed2717e23bee5c4fcea098409911932de43
Zooko Ofsimplegeo <zooko@simplegeo.com>
2010-11-19 11:05:30 -0700

    copy in the constructors of Record from python-simplegeo, causing test_record_from_dict() to go green also add simplegeo/places/test/test_record.py~, just because there is something screwed up in git and this might fix it:
    You have uncommitted changes in your source tree:

--- __init__.py
+++ __init__.py
@@ -7,11 +7,35 @@ except ImportError:
 
 API_VERSION = '0.1'
 
+import time
+
 class Client:
     pass
 
 class Record:
-    pass
+    def __init__(self, layer, id, lat, lon, type='object', created=None, **kwargs):
+        self.layer = layer
+        self.id = id
+        self.lon = lon
+        self.lat = lat
+        self.type = type
+        if created is None:
+            self.created = int(time.time())
+        else:
+            self.created = created
+        self.__dict__.update(kwargs)
+
+    @classmethod
+    def from_dict(cls, data):
+        if not data:
+            return None
+        coord = data['geometry']['coordinates']
+        record = cls(data['properties']['layer'], data['id'], lat=coord[1], lon=coord[0])
+        record.type = data['properties']['type']
+        record.created = data.get('created', record.created)
+        record.__dict__.update(dict((k, v) for k, v in data['properties'].iteritems()
+                                    if k not in ('layer', 'type', 'created')))
+        return record
 
 class APIError:
     pass
