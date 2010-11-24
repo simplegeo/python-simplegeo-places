9412faa2ceee3cac0eeca10b5dcaae1a8267df23
Zooko Ofsimplegeo <zooko@simplegeo.com>
2010-11-24 14:20:31 -0700

    add search(), remove notion of a 'layer', add TODO.rst

--- simplegeo/places/test/test_record.py
+++ simplegeo/places/test/test_record.py
@@ -8,24 +8,21 @@ class RecordTest(unittest.TestCase):
     @mock.patch('time.time')
     def test_record_constructor(self, mock_time):
         mock_time.return_value = 100
-        record = Record('my_layer', 'my_id', D('11.0'), D('10.0'))
-        self.failUnlessEqual(record.layer, 'my_layer')
+        record = Record('my_id', D('11.0'), D('10.0'))
         self.failUnlessEqual(record.id, 'my_id')
         self.failUnlessEqual(record.lat, D('11.0'))
         self.failUnlessEqual(record.lon, D('10.0'))
         self.failUnlessEqual(record.created, 100)
 
         mock_time.return_value = 101
-        record = Record('my_layer', 'my_id', D('11.0'), D('10.0'), created=102)
-        self.failUnlessEqual(record.layer, 'my_layer')
+        record = Record('my_id', D('11.0'), D('10.0'), created=102)
         self.failUnlessEqual(record.id, 'my_id')
         self.failUnlessEqual(record.lat, D('11.0'))
         self.failUnlessEqual(record.lon, D('10.0'))
         self.failUnlessEqual(record.created, 102)
 
         mock_time.return_value = 103
-        record = Record('my_layer', 'my_id', 11.0, 10.0)
-        self.failUnlessEqual(record.layer, 'my_layer')
+        record = Record('my_id', 11.0, 10.0)
         self.failUnlessEqual(record.id, 'my_id')
         self.failUnlessEqual(record.lat, 11.0)
         self.failUnlessEqual(record.lon, 10.0)
@@ -42,7 +39,6 @@ class RecordTest(unittest.TestCase):
                      'id' : 'my_id',
                      'type' : 'Feature',
                      'properties' : {
-                                     'layer' : 'my_layer',
                                      'key' : 'value'  ,
                                      'type' : 'object'
                                      }
@@ -52,7 +48,6 @@ class RecordTest(unittest.TestCase):
         self.assertEquals(record.lat, D('11.0'))
         self.assertEquals(record.lon, D('10.0'))
         self.assertEquals(record.id, 'my_id')
-        self.assertEquals(record.layer, 'my_layer')
         self.assertEquals(record.key, 'value')
         self.assertEquals(record.type, 'object')
         self.assertEquals(record.created, 99)
@@ -65,7 +60,6 @@ class RecordTest(unittest.TestCase):
                      'id' : 'my_id',
                      'type' : 'Feature',
                      'properties' : {
-                                     'layer' : 'my_layer',
                                      'key' : 'value'  ,
                                      'type' : 'object'
                                      }
@@ -75,12 +69,11 @@ class RecordTest(unittest.TestCase):
         self.assertEquals(record.lat, D('11.0'))
         self.assertEquals(record.lon, D('10.03'))
         self.assertEquals(record.id, 'my_id')
-        self.assertEquals(record.layer, 'my_layer')
         self.assertEquals(record.key, 'value')
         self.assertEquals(record.type, 'object')
         self.assertEquals(record.created, 98)
 
-        self.assertEquals('{"geometry": {"type": "Point", "coordinates": [10.03, 11.0]}, "properties": {"layer": "my_layer", "type": "object", "key": "value"}, "type": "Feature", "id": "my_id", "created": 98}', record.to_json())
+        self.assertEquals('{"geometry": {"type": "Point", "coordinates": [10.03, 11.0]}, "properties": {"type": "object", "key": "value"}, "type": "Feature", "id": "my_id", "created": 98}', record.to_json())
 
         record_dict = { 'created' : 97,
                      'geometry' : {
@@ -90,7 +83,6 @@ class RecordTest(unittest.TestCase):
                      'id' : 'my_id',
                      'type' : 'Feature',
                      'properties' : {
-                                     'layer' : 'my_layer',
                                      'key' : 'value'  ,
                                      'type' : 'object'
                                      }
@@ -100,9 +92,8 @@ class RecordTest(unittest.TestCase):
         self.assertEquals(record.lat, 11.0)
         self.assertEquals(record.lon, 10.0)
         self.assertEquals(record.id, 'my_id')
-        self.assertEquals(record.layer, 'my_layer')
         self.assertEquals(record.key, 'value')
         self.assertEquals(record.type, 'object')
         self.assertEquals(record.created, 97)
 
-        self.assertEquals('{"geometry": {"type": "Point", "coordinates": [10.0, 11.0]}, "properties": {"layer": "my_layer", "type": "object", "key": "value"}, "type": "Feature", "id": "my_id", "created": 97}', record.to_json())
+        self.assertEquals('{"geometry": {"type": "Point", "coordinates": [10.0, 11.0]}, "properties": {"type": "object", "key": "value"}, "type": "Feature", "id": "my_id", "created": 97}', record.to_json())
