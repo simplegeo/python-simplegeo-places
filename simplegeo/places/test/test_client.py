1eb2f20fff929ca18e58dc16a9342ba12be90fea
Zooko Ofsimplegeo <zooko@simplegeo.com>
2010-11-24 13:56:36 -0700

    add update_record()

--- simplegeo/places/test/test_client.py
+++ simplegeo/places/test/test_client.py
@@ -19,7 +19,14 @@ API_VERSION = '1.0'
 API_HOST = 'api.simplegeo.com'
 API_PORT = 80
 
-ENDPOINT_DESCR=json.dumps({"GET /1.0/places/<place_id:[a-zA-Z0-9\\.,_-]+>.json": "Return a record for a place.", "GET /1.0/endpoints.json": "Describe known endpoints.", "POST /1.0/places/<place_id:.*>.json": "Update a record.", "GET /1.0/places/<lat:-?[0-9\\.]+>,<lon:-?[0-9\\.]+>/search.json": "Search for places near a lat/lon.", "PUT /1.0/places/place.json": "Create a new record, returns a 301 to the location of the resource.", "GET /1.0/debug/<number:\\d+>": "Undocumented.", "DELETE /1.0/places/<place_id:.*>.json": "Delete a record."})
+ENDPOINT_DESCR=json.dumps({
+        "GET /1.0/places/<place_id:[a-zA-Z0-9\\.,_-]+>.json": "Return a record for a place.",
+        "GET /1.0/endpoints.json": "Describe known endpoints.",
+        "POST /1.0/places/<place_id:.*>.json": "Update a record.",
+        "GET /1.0/places/<lat:-?[0-9\\.]+>,<lon:-?[0-9\\.]+>/search.json": "Search for places near a lat/lon.",
+        "PUT /1.0/places/place.json": "Create a new record, returns a 301 to the location of the resource.",
+        "GET /1.0/debug/<number:\\d+>": "Undocumented.",
+        "DELETE /1.0/places/<place_id:.*>.json": "Delete a record."})
 
 class ClientTest(unittest.TestCase):
     def setUp(self):
@@ -80,6 +87,17 @@ class ClientTest(unittest.TestCase):
         self.assertEqual(mockhttp.method_calls[0][1][1], 'GET')
         self.failUnless(isinstance(res, Record), res)
         self.assertEqual(res.to_json(), resultrecord.to_json())
+
+    def test_update_record(self):
+        simplegeoid = 'abcdefghijklmnopqrstuvwxyz'
+        rec = Record('a_layer', simplegeoid, D('11.03'), D('10.04'))
+
+        mockhttp = mock.Mock()
+        mockhttp.request.return_value = ({'status': '200', 'content-type': 'application/json', }, rec.to_json())
+        self.client.http = mockhttp
+
+        res = self.client.update_record(rec)
+        self.failUnlessEqual(res.to_json(), rec.to_json())
 
     def DISABLED_test_multi_record_post(self):
         feats = [self._record() for i in range(10)]
