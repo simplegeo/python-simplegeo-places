79297eadd45fd3a238fa8d826ff3cfe91e65bd47
Simple Geebus <nerds+geebus@simplegeo.com>
2010-12-07 19:51:55 +0000

    Prepared changelog for build 66

--- simplegeo/places/_version.py
+++ simplegeo/places/_version.py
@@ -1,6 +1,6 @@
 # This is the version of this source code.
 
-verstr = "1.3.65"
+verstr = "1.3.66"
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
