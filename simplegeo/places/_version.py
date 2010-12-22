725202e00b9c9909bd1bcd98c8b9af3775460535
Simple Geebus <nerds+geebus@simplegeo.com>
2010-12-22 16:37:45 +0000

    Prepared changelog for build 83

--- simplegeo/places/_version.py
+++ simplegeo/places/_version.py
@@ -1,6 +1,6 @@
 # This is the version of this source code.
 
-verstr = "2.0.82"
+verstr = "2.0.83"
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
