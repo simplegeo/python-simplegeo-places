568df1e78ce3c4de90c2b439bb8dc892a1ee39f9
Zooko Ofsimplegeo <zooko@simplegeo.com>
2010-12-22 09:39:11 -0700

    split version into manual and automatic components

--- _version.py
+++ _version.py
@@ -1,6 +1,14 @@
 # This is the version of this source code.
 
-verstr = "2.0.82"
+manual_verstr = "2.0"
+
+
+
+auto_build_num = "82"
+
+
+
+verstr = manual_verstr + "." + auto_build_num
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
